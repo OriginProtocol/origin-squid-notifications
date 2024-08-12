5e62516d276f6b4d40dde2aaa2416f24fc888b6d
Chris Jacobs <apexearth@gmail.com>
2024-08-12 10:44:13 -0700

    feat: oeth withdrawals - update otoken vault abi to capture the new events

--- src/abi/otoken-vault.ts
+++ src/abi/otoken-vault.ts
@@ -6,9 +6,11 @@ export const events = {
     AllocateThresholdUpdated: event("0x2ec5fb5a3d2703edc461252d92ccd2799c3c74f01d97212b20388207fa17ae45", "AllocateThresholdUpdated(uint256)", {"_threshold": p.uint256}),
     AssetAllocated: event("0x41b99659f6ba0803f444aff29e5bf6e26dd86a3219aff92119d69710a956ba8d", "AssetAllocated(address,address,uint256)", {"_asset": p.address, "_strategy": p.address, "_amount": p.uint256}),
     AssetDefaultStrategyUpdated: event("0xba58ce12801c949fa65f41c46ed108671c219baf945fa48d21026cea99ff252a", "AssetDefaultStrategyUpdated(address,address)", {"_asset": p.address, "_strategy": p.address}),
+    AssetRemoved: event("0x37803e2125c48ee96c38ddf04e826daf335b0e1603579040fd275aba6d06b6fc", "AssetRemoved(address)", {"_asset": p.address}),
     AssetSupported: event("0x4f1ac48525e50059cc1cc6e0e1940ece0dd653a4db4841538d6aef036be2fb7b", "AssetSupported(address)", {"_asset": p.address}),
     CapitalPaused: event("0x71f0e5b62f846a22e0b4d159e516e62fa9c2b8eb570be15f83e67d98a2ee51e0", "CapitalPaused()", {}),
     CapitalUnpaused: event("0x891ebab18da80ebeeea06b1b1cede098329c4c008906a98370c2ac7a80b571cb", "CapitalUnpaused()", {}),
+    DripperChanged: event("0xaf2910d9759321733de15af1827a49830692912adeb2b3646334861f2cd2eed4", "DripperChanged(address)", {"_dripper": indexed(p.address)}),
     GovernorshipTransferred: event("0xc7c0c772add429241571afb3805861fb3cfa2af374534088b76cdb4325a87e9a", "GovernorshipTransferred(address,address)", {"previousGovernor": indexed(p.address), "newGovernor": indexed(p.address)}),
     MaxSupplyDiffChanged: event("0x95201f9c21f26877223b1ff4073936a6484c35495649e60e55730497aeb60d93", "MaxSupplyDiffChanged(uint256)", {"maxSupplyDiff": p.uint256}),
     Mint: event("0x0f6798a560793a54c3bcfe86a93cde1e73087d944c0ea20544137d4121396885", "Mint(address,uint256)", {"_addr": p.address, "_value": p.uint256}),
@@ -22,8 +24,10 @@ export const events = {
     Redeem: event("0x222838db2794d11532d940e8dec38ae307ed0b63cd97c233322e221f998767a6", "Redeem(address,uint256)", {"_addr": p.address, "_value": p.uint256}),
     RedeemFeeUpdated: event("0xd6c7508d6658ccee36b7b7d7fd72e5cbaeefb40c64eff24e9ae7470e846304ee", "RedeemFeeUpdated(uint256)", {"_redeemFeeBps": p.uint256}),
     StrategistUpdated: event("0x869e0abd13cc3a975de7b93be3df1cb2255c802b1cead85963cc79d99f131bee", "StrategistUpdated(address)", {"_address": p.address}),
+    StrategyAddedToMintWhitelist: event("0x47c8c96a5942f094264111c5fe7f6a4fe86efe63254a6fa7afa5fc84f07d58e8", "StrategyAddedToMintWhitelist(address)", {"strategy": indexed(p.address)}),
     StrategyApproved: event("0x960dd94cbb79169f09a4e445d58b895df2d9bffa5b31055d0932d801724a20d1", "StrategyApproved(address)", {"_addr": p.address}),
     StrategyRemoved: event("0x09a1db4b80c32706328728508c941a6b954f31eb5affd32f236c1fd405f8fea4", "StrategyRemoved(address)", {"_addr": p.address}),
+    StrategyRemovedFromMintWhitelist: event("0x0ec40967a61509853550658e51d0e4297f7cba244fe4adc8ba18b5631ac20e2f", "StrategyRemovedFromMintWhitelist(address)", {"strategy": indexed(p.address)}),
     SwapAllowedUndervalueChanged: event("0xf12c00256bee2b6facb111a88a9b1cff86e79132939b44f1353212d6f7469557", "SwapAllowedUndervalueChanged(uint256)", {"_basis": p.uint256}),
     SwapSlippageChanged: event("0x8d22e9d2cbe8bb65a3c4412bd8970743864512a1a0e004e8d00fb96277b78b94", "SwapSlippageChanged(address,uint256)", {"_asset": p.address, "_basis": p.uint256}),
     Swapped: event("0xa078c4190abe07940190effc1846be0ccf03ad6007bc9e93f9697d0b460befbb", "Swapped(address,address,uint256,uint256)", {"_fromAsset": indexed(p.address), "_toAsset": indexed(p.address), "_fromAssetAmount": p.uint256, "_toAssetAmount": p.uint256}),
@@ -31,25 +35,36 @@ export const events = {
     TrusteeAddressChanged: event("0x1e4af5ac389e8cde1bdaa6830881b6c987c62a45cfb3b33d27d805cde3b57750", "TrusteeAddressChanged(address)", {"_address": p.address}),
     TrusteeFeeBpsChanged: event("0x56287a45051933ea374811b3d5d165033047be5572cac676f7c28b8be4f746c7", "TrusteeFeeBpsChanged(uint256)", {"_basis": p.uint256}),
     VaultBufferUpdated: event("0x41ecb23a0e7865b25f38c268b7c3012220d822929e9edff07326e89d5bb822b5", "VaultBufferUpdated(uint256)", {"_vaultBuffer": p.uint256}),
+    WithdrawalClaimable: event("0xee79a0c43d3993055690b54e074b5153e8bae8d1a872b656dedb64aa8f463333", "WithdrawalClaimable(uint256,uint256)", {"_claimable": p.uint256, "_newClaimable": p.uint256}),
+    WithdrawalClaimed: event("0x2d43eb174787155132b52ddb6b346e2dca99302eac3df4466dbeff953d3c84d1", "WithdrawalClaimed(address,uint256,uint256)", {"_withdrawer": indexed(p.address), "_requestId": indexed(p.uint256), "_amount": p.uint256}),
+    WithdrawalRequested: event("0x38e3d972947cfef94205163d483d6287ef27eb312e20cb8e0b13a49989db232e", "WithdrawalRequested(address,uint256,uint256,uint256)", {"_withdrawer": indexed(p.address), "_requestId": indexed(p.uint256), "_amount": p.uint256, "_queued": p.uint256}),
     YieldDistribution: event("0x09516ecf4a8a86e59780a9befc6dee948bc9e60a36e3be68d31ea817ee8d2c80", "YieldDistribution(address,uint256,uint256)", {"_to": p.address, "_yield": p.uint256, "_fee": p.uint256}),
 }
 
 export const functions = {
+    CLAIM_DELAY: viewFun("0xe18132c4", "CLAIM_DELAY()", {}, p.uint256),
+    addWithdrawalQueueLiquidity: fun("0xb9b17f9f", "addWithdrawalQueueLiquidity()", {}, ),
     allocate: fun("0xabaa9916", "allocate()", {}, ),
     assetDefaultStrategies: viewFun("0xa403e4d5", "assetDefaultStrategies(address)", {"_0": p.address}, p.address),
     autoAllocateThreshold: viewFun("0x9fa1826e", "autoAllocateThreshold()", {}, p.uint256),
     burnForStrategy: fun("0x6217f3ea", "burnForStrategy(uint256)", {"_amount": p.uint256}, ),
+    cacheWETHAssetIndex: fun("0x44c54707", "cacheWETHAssetIndex()", {}, ),
     calculateRedeemOutputs: viewFun("0x67bd7ba3", "calculateRedeemOutputs(uint256)", {"_amount": p.uint256}, p.array(p.uint256)),
     capitalPaused: viewFun("0xe6cc5432", "capitalPaused()", {}, p.bool),
     checkBalance: viewFun("0x5f515226", "checkBalance(address)", {"_asset": p.address}, p.uint256),
     claimGovernance: fun("0x5d36b190", "claimGovernance()", {}, ),
+    claimWithdrawal: fun("0xf8444436", "claimWithdrawal(uint256)", {"_requestId": p.uint256}, p.uint256),
+    claimWithdrawals: fun("0x48e30f54", "claimWithdrawals(uint256[])", {"_requestIds": p.array(p.uint256)}, {"amounts": p.array(p.uint256), "totalAmount": p.uint256}),
+    dripper: viewFun("0x603ea03b", "dripper()", {}, p.address),
     getAllAssets: viewFun("0x2acada4d", "getAllAssets()", {}, p.array(p.address)),
     getAllStrategies: viewFun("0xc3b28864", "getAllStrategies()", {}, p.array(p.address)),
     getAssetConfig: viewFun("0x6ec3ab67", "getAssetConfig(address)", {"_asset": p.address}, p.struct({"isSupported": p.bool, "unitConversion": p.uint8, "decimals": p.uint8, "allowedOracleSlippageBps": p.uint16})),
     getAssetCount: viewFun("0xa0aead4d", "getAssetCount()", {}, p.uint256),
     getStrategyCount: viewFun("0x31e19cfa", "getStrategyCount()", {}, p.uint256),
     governor: viewFun("0x0c340a24", "governor()", {}, p.address),
+    initialize: fun("0x485cc955", "initialize(address,address)", {"_priceProvider": p.address, "_oToken": p.address}, ),
     isGovernor: viewFun("0xc7af3352", "isGovernor()", {}, p.bool),
+    isMintWhitelistedStrategy: viewFun("0x4530820a", "isMintWhitelistedStrategy(address)", {"_0": p.address}, p.bool),
     isSupportedAsset: viewFun("0x9be918e6", "isSupportedAsset(address)", {"_asset": p.address}, p.bool),
     maxSupplyDiff: viewFun("0x8e510b52", "maxSupplyDiff()", {}, p.uint256),
     mint: fun("0x156e29f6", "mint(address,uint256,uint256)", {"_asset": p.address, "_amount": p.uint256, "_minimumOusdAmount": p.uint256}, ),
@@ -66,6 +81,7 @@ export const functions = {
     redeem: fun("0x7cbc2373", "redeem(uint256,uint256)", {"_amount": p.uint256, "_minimumUnitAmount": p.uint256}, ),
     redeemAll: fun("0x7136a7a6", "redeemAll(uint256)", {"_minimumUnitAmount": p.uint256}, ),
     redeemFeeBps: viewFun("0x09f6442c", "redeemFeeBps()", {}, p.uint256),
+    requestWithdrawal: fun("0x9ee679e8", "requestWithdrawal(uint256)", {"_amount": p.uint256}, {"requestId": p.uint256, "queued": p.uint256}),
     setAdminImpl: fun("0xfc0cfeee", "setAdminImpl(address)", {"newImpl": p.address}, ),
     strategistAddr: viewFun("0x570d8e1d", "strategistAddr()", {}, p.address),
     totalValue: viewFun("0xd4c3eea0", "totalValue()", {}, p.uint256),
@@ -73,9 +89,17 @@ export const functions = {
     trusteeAddress: viewFun("0x49c1d54d", "trusteeAddress()", {}, p.address),
     trusteeFeeBps: viewFun("0x207134b0", "trusteeFeeBps()", {}, p.uint256),
     vaultBuffer: viewFun("0x1edfe3da", "vaultBuffer()", {}, p.uint256),
+    weth: viewFun("0x3fc8cef3", "weth()", {}, p.address),
+    wethAssetIndex: viewFun("0x54c6d858", "wethAssetIndex()", {}, p.uint256),
+    withdrawalQueueMetadata: viewFun("0x362bd1a3", "withdrawalQueueMetadata()", {}, {"queued": p.uint128, "claimable": p.uint128, "claimed": p.uint128, "nextWithdrawalIndex": p.uint128}),
+    withdrawalRequests: viewFun("0x937b2581", "withdrawalRequests(uint256)", {"_0": p.uint256}, {"withdrawer": p.address, "claimed": p.bool, "timestamp": p.uint40, "amount": p.uint128, "queued": p.uint128}),
 }
 
 export class Contract extends ContractBase {
+
+    CLAIM_DELAY() {
+        return this.eth_call(functions.CLAIM_DELAY, {})
+    }
 
     assetDefaultStrategies(_0: AssetDefaultStrategiesParams["_0"]) {
         return this.eth_call(functions.assetDefaultStrategies, {_0})
@@ -95,6 +119,10 @@ export class Contract extends ContractBase {
 
     checkBalance(_asset: CheckBalanceParams["_asset"]) {
         return this.eth_call(functions.checkBalance, {_asset})
+    }
+
+    dripper() {
+        return this.eth_call(functions.dripper, {})
     }
 
     getAllAssets() {
@@ -123,6 +151,10 @@ export class Contract extends ContractBase {
 
     isGovernor() {
         return this.eth_call(functions.isGovernor, {})
+    }
+
+    isMintWhitelistedStrategy(_0: IsMintWhitelistedStrategyParams["_0"]) {
+        return this.eth_call(functions.isMintWhitelistedStrategy, {_0})
     }
 
     isSupportedAsset(_asset: IsSupportedAssetParams["_asset"]) {
@@ -188,15 +220,33 @@ export class Contract extends ContractBase {
     vaultBuffer() {
         return this.eth_call(functions.vaultBuffer, {})
     }
+
+    weth() {
+        return this.eth_call(functions.weth, {})
+    }
+
+    wethAssetIndex() {
+        return this.eth_call(functions.wethAssetIndex, {})
+    }
+
+    withdrawalQueueMetadata() {
+        return this.eth_call(functions.withdrawalQueueMetadata, {})
+    }
+
+    withdrawalRequests(_0: WithdrawalRequestsParams["_0"]) {
+        return this.eth_call(functions.withdrawalRequests, {_0})
+    }
 }
 
 /// Event types
 export type AllocateThresholdUpdatedEventArgs = EParams<typeof events.AllocateThresholdUpdated>
 export type AssetAllocatedEventArgs = EParams<typeof events.AssetAllocated>
 export type AssetDefaultStrategyUpdatedEventArgs = EParams<typeof events.AssetDefaultStrategyUpdated>
+export type AssetRemovedEventArgs = EParams<typeof events.AssetRemoved>
 export type AssetSupportedEventArgs = EParams<typeof events.AssetSupported>
 export type CapitalPausedEventArgs = EParams<typeof events.CapitalPaused>
 export type CapitalUnpausedEventArgs = EParams<typeof events.CapitalUnpaused>
+export type DripperChangedEventArgs = EParams<typeof events.DripperChanged>
 export type GovernorshipTransferredEventArgs = EParams<typeof events.GovernorshipTransferred>
 export type MaxSupplyDiffChangedEventArgs = EParams<typeof events.MaxSupplyDiffChanged>
 export type MintEventArgs = EParams<typeof events.Mint>
@@ -210,8 +260,10 @@ export type RebaseUnpausedEventArgs = EParams<typeof events.RebaseUnpaused>
 export type RedeemEventArgs = EParams<typeof events.Redeem>
 export type RedeemFeeUpdatedEventArgs = EParams<typeof events.RedeemFeeUpdated>
 export type StrategistUpdatedEventArgs = EParams<typeof events.StrategistUpdated>
+export type StrategyAddedToMintWhitelistEventArgs = EParams<typeof events.StrategyAddedToMintWhitelist>
 export type StrategyApprovedEventArgs = EParams<typeof events.StrategyApproved>
 export type StrategyRemovedEventArgs = EParams<typeof events.StrategyRemoved>
+export type StrategyRemovedFromMintWhitelistEventArgs = EParams<typeof events.StrategyRemovedFromMintWhitelist>
 export type SwapAllowedUndervalueChangedEventArgs = EParams<typeof events.SwapAllowedUndervalueChanged>
 export type SwapSlippageChangedEventArgs = EParams<typeof events.SwapSlippageChanged>
 export type SwappedEventArgs = EParams<typeof events.Swapped>
@@ -219,9 +271,18 @@ export type SwapperChangedEventArgs = EParams<typeof events.SwapperChanged>
 export type TrusteeAddressChangedEventArgs = EParams<typeof events.TrusteeAddressChanged>
 export type TrusteeFeeBpsChangedEventArgs = EParams<typeof events.TrusteeFeeBpsChanged>
 export type VaultBufferUpdatedEventArgs = EParams<typeof events.VaultBufferUpdated>
+export type WithdrawalClaimableEventArgs = EParams<typeof events.WithdrawalClaimable>
+export type WithdrawalClaimedEventArgs = EParams<typeof events.WithdrawalClaimed>
+export type WithdrawalRequestedEventArgs = EParams<typeof events.WithdrawalRequested>
 export type YieldDistributionEventArgs = EParams<typeof events.YieldDistribution>
 
 /// Function types
+export type CLAIM_DELAYParams = FunctionArguments<typeof functions.CLAIM_DELAY>
+export type CLAIM_DELAYReturn = FunctionReturn<typeof functions.CLAIM_DELAY>
+
+export type AddWithdrawalQueueLiquidityParams = FunctionArguments<typeof functions.addWithdrawalQueueLiquidity>
+export type AddWithdrawalQueueLiquidityReturn = FunctionReturn<typeof functions.addWithdrawalQueueLiquidity>
+
 export type AllocateParams = FunctionArguments<typeof functions.allocate>
 export type AllocateReturn = FunctionReturn<typeof functions.allocate>
 
@@ -234,6 +295,9 @@ export type AutoAllocateThresholdReturn = FunctionReturn<typeof functions.autoAl
 export type BurnForStrategyParams = FunctionArguments<typeof functions.burnForStrategy>
 export type BurnForStrategyReturn = FunctionReturn<typeof functions.burnForStrategy>
 
+export type CacheWETHAssetIndexParams = FunctionArguments<typeof functions.cacheWETHAssetIndex>
+export type CacheWETHAssetIndexReturn = FunctionReturn<typeof functions.cacheWETHAssetIndex>
+
 export type CalculateRedeemOutputsParams = FunctionArguments<typeof functions.calculateRedeemOutputs>
 export type CalculateRedeemOutputsReturn = FunctionReturn<typeof functions.calculateRedeemOutputs>
 
@@ -245,6 +309,15 @@ export type CheckBalanceReturn = FunctionReturn<typeof functions.checkBalance>
 
 export type ClaimGovernanceParams = FunctionArguments<typeof functions.claimGovernance>
 export type ClaimGovernanceReturn = FunctionReturn<typeof functions.claimGovernance>
+
+export type ClaimWithdrawalParams = FunctionArguments<typeof functions.claimWithdrawal>
+export type ClaimWithdrawalReturn = FunctionReturn<typeof functions.claimWithdrawal>
+
+export type ClaimWithdrawalsParams = FunctionArguments<typeof functions.claimWithdrawals>
+export type ClaimWithdrawalsReturn = FunctionReturn<typeof functions.claimWithdrawals>
+
+export type DripperParams = FunctionArguments<typeof functions.dripper>
+export type DripperReturn = FunctionReturn<typeof functions.dripper>
 
 export type GetAllAssetsParams = FunctionArguments<typeof functions.getAllAssets>
 export type GetAllAssetsReturn = FunctionReturn<typeof functions.getAllAssets>
@@ -264,8 +337,14 @@ export type GetStrategyCountReturn = FunctionReturn<typeof functions.getStrategy
 export type GovernorParams = FunctionArguments<typeof functions.governor>
 export type GovernorReturn = FunctionReturn<typeof functions.governor>
 
+export type InitializeParams = FunctionArguments<typeof functions.initialize>
+export type InitializeReturn = FunctionReturn<typeof functions.initialize>
+
 export type IsGovernorParams = FunctionArguments<typeof functions.isGovernor>
 export type IsGovernorReturn = FunctionReturn<typeof functions.isGovernor>
+
+export type IsMintWhitelistedStrategyParams = FunctionArguments<typeof functions.isMintWhitelistedStrategy>
+export type IsMintWhitelistedStrategyReturn = FunctionReturn<typeof functions.isMintWhitelistedStrategy>
 
 export type IsSupportedAssetParams = FunctionArguments<typeof functions.isSupportedAsset>
 export type IsSupportedAssetReturn = FunctionReturn<typeof functions.isSupportedAsset>
@@ -315,6 +394,9 @@ export type RedeemAllReturn = FunctionReturn<typeof functions.redeemAll>
 export type RedeemFeeBpsParams = FunctionArguments<typeof functions.redeemFeeBps>
 export type RedeemFeeBpsReturn = FunctionReturn<typeof functions.redeemFeeBps>
 
+export type RequestWithdrawalParams = FunctionArguments<typeof functions.requestWithdrawal>
+export type RequestWithdrawalReturn = FunctionReturn<typeof functions.requestWithdrawal>
+
 export type SetAdminImplParams = FunctionArguments<typeof functions.setAdminImpl>
 export type SetAdminImplReturn = FunctionReturn<typeof functions.setAdminImpl>
 
@@ -335,4 +417,16 @@ export type TrusteeFeeBpsReturn = FunctionReturn<typeof functions.trusteeFeeBps>
 
 export type VaultBufferParams = FunctionArguments<typeof functions.vaultBuffer>
 export type VaultBufferReturn = FunctionReturn<typeof functions.vaultBuffer>
+
+export type WethParams = FunctionArguments<typeof functions.weth>
+export type WethReturn = FunctionReturn<typeof functions.weth>
+
+export type WethAssetIndexParams = FunctionArguments<typeof functions.wethAssetIndex>
+export type WethAssetIndexReturn = FunctionReturn<typeof functions.wethAssetIndex>
+
+export type WithdrawalQueueMetadataParams = FunctionArguments<typeof functions.withdrawalQueueMetadata>
+export type WithdrawalQueueMetadataReturn = FunctionReturn<typeof functions.withdrawalQueueMetadata>
+
+export type WithdrawalRequestsParams = FunctionArguments<typeof functions.withdrawalRequests>
+export type WithdrawalRequestsReturn = FunctionReturn<typeof functions.withdrawalRequests>
 
