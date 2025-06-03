bfd562fd38c6df0092f485264cabe57551741adf
Chris Jacobs <apexearth@gmail.com>
2025-06-02 18:40:52 -0700

    add sorting to digest generation

--- src/digest.ts
+++ src/digest.ts
@@ -8,7 +8,11 @@ console.log('# Origin Squid Notifications - Digest')
 console.log(new Date().toLocaleString())
 console.log('=========================================')
 load().then((processors) => {
-  for (const p of processors.sort((a, b) => a.chainId - b.chainId)) {
+  for (const p of processors.sort((a, b) => {
+    if (a.topic !== b.topic) return a.topic > b.topic ? 1 : -1
+    if (a.chainId !== b.chainId) return a.chainId - b.chainId
+    return (a.name ?? '') > (b.name ?? '') ? 1 : -1
+  })) {
     console.log('=========================================')
     console.log(formatJson(p))
   }
