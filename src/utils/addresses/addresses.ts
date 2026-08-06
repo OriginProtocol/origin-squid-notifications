e3e440cbdda59ffb7373285349529b25befe4804
Nicholas Addison <nick@addisonbrown.com.au>
2026-08-06 14:21:48 +1000

    Added more token mappings

--- src/utils/addresses/addresses.ts
+++ src/utils/addresses/addresses.ts
@@ -14,12 +14,15 @@ export const LUSD_ADDRESS = '0x5f98805a4e8be255a32880fdec7f6728c6568ba0'
 export const USDS_ADDRESS = '0xdc035d45d973e3ec169d2276ddab16f1e407384f'
 export const USDE_ADDRESS = '0x4c9edd5852cd905f086c759e8383e09bff1e68b3'
 export const SUSDE_ADDRESS = '0x9d39a5de30e57443bff2a8307a4256c8797a3497'
+export const USDG_ADDRESS = '0xe343167631d89b6ffc58b88d6b7fb0228795491d'
+export const PYUSD_ADDRESS = '0x6c3ea9036406852006290770bedfcaba0e23a0e8'
 
 // ETH Stables
 export const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
 export const STETH_ADDRESS = '0xae7ab96520de3a18e5e111b5eaab095312d7fe84'
 export const WSTETH_ADDRESS = '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0'
 export const EETH_ADDRESS = '0x35fa164735182de50811e8e2e824cfb9b6118ac2'
+export const WEETH_ADDRESS = '0xcd5fe23c85820f7b72d0926fc9b05b43e359b7ee'
 export const RETH_ADDRESS = '0xae78736cd615f374d3085123a210448e74fc6393'
 export const FRXETH_ADDRESS = '0x5e8422345238f34275888049021821e8e08caa1f'
 export const SFRXETH_ADDRESS = '0xac3e018457b222d93114458476f3e3416abbe38f'
@@ -155,6 +158,7 @@ export const tokens = {
   WETH: WETH_ADDRESS,
   stETH: STETH_ADDRESS,
   eETH: EETH_ADDRESS,
+  weETH: WEETH_ADDRESS,
   wstETH: WSTETH_ADDRESS,
   rETH: RETH_ADDRESS,
   frxETH: FRXETH_ADDRESS,
@@ -167,6 +171,8 @@ export const tokens = {
   // Ethena
   USDe: USDE_ADDRESS,
   sUSDe: SUSDE_ADDRESS,
+  USDG: USDG_ADDRESS,
+  PYUSD: PYUSD_ADDRESS,
 } as const
 export type TokenSymbol = keyof typeof tokens
 export type TokenAddress = (typeof tokens)[TokenSymbol]
